ec0d0557ceb0cbbb20fd9580c81066411cf628fc
Wesley Ribeiro <wes.rsilva@gmail.com>
2020-04-25 11:41:47 -0300

    Adicionar testes com números inválidos

--- aprendendo/coding_dojo/bank_ocr/tests/test_valid_account_numbers.py
+++ aprendendo/coding_dojo/bank_ocr/tests/test_valid_account_numbers.py
@@ -8,6 +8,12 @@ class TestValidAccountNumbers(unittest.TestCase):
     def test_should_get_true_with_account_numbers_valid(self):
         self.assertTrue(ValidatorAccountNumbers.is_valid('457508000'))
 
+    def test_should_get_false_with_account_numbers_all_number_one(self):
+        self.assertFalse(ValidatorAccountNumbers.is_valid('111111111'))
+
+    def test_should_get_false_with_account_numbers_invalid(self):
+        self.assertFalse(ValidatorAccountNumbers.is_valid('664371495'))
+
 
 if __name__ == '__main__':
     unittest.main()
