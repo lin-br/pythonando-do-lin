63d20d0dbaf4a3fdc823e9cd7545754952fb22df
Wesley Ribeiro <wes.rsilva@gmail.com>
2020-04-25 21:28:17 -0300

    Implementar testes do relatório

---
+++ aprendendo/coding_dojo/bank_ocr/src/ocr/report.py
@@ -0,0 +1,14 @@
+from ocr.reader_account_numbers import ReaderAccountNumber
+from ocr.validator_account_numbers import ValidatorAccountNumbers
+
+
+class Report:
+    @staticmethod
+    def get_report_in_line(text):
+        reader = ReaderAccountNumber(text)
+        if '?' in reader.numbers:
+            return f"{reader.numbers} ILL"
+        elif not ValidatorAccountNumbers.is_valid(reader.numbers):
+            return f"{reader.numbers} ERR"
+        else:
+            return reader.numbers
---
+++ aprendendo/coding_dojo/bank_ocr/tests/test_accounts_numbers_report.py
@@ -0,0 +1,38 @@
+import unittest
+
+from ocr.report import Report
+
+
+class TestAccountsNumbersReport(unittest.TestCase):
+
+    def test_should_get_line_without_message_attemption(self):
+        text = ' _  _  _  _  _  _  _  _    \n'
+        text += '| || || || || || || ||_   |\n'
+        text += '|_||_||_||_||_||_||_| _|  |\n'
+        text += '                           '
+        self.assertEqual('000000051', Report.get_report_in_line(text))
+
+    def test_should_get_line_with_flag_ERR(self):
+        text = ' _  _     _  _        _  _ \n'
+        text += '|_ |_ |_| _|  |  ||_||_||_ \n'
+        text += '|_||_|  | _|  |  |  | _| _|\n'
+        text += '                           '
+        self.assertEqual('664371495 ERR', Report.get_report_in_line(text))
+
+    def test_should_get_line_with_flag_ILL_and_in_an_unidentified_digit_a_question_mark(self):
+        text = '    _  _  _  _  _  _     _ \n'
+        text += '|_||_|| || ||_   |  |  | _ \n'
+        text += '  | _||_||_||_|  |  |  | _|\n'
+        text += '                           '
+        self.assertEqual('49006771? ILL', Report.get_report_in_line(text))
+
+    def test_should_get_line_with_flag_ILL_and_in_the_unidentified_digits_a_question_mark(self):
+        text = '    _  _     _  _  _  _  _ \n'
+        text += '  | _| _||_| _ |_   ||_||_|\n'
+        text += '  ||_  _|  | _||_|  ||_| _ \n'
+        text += '                           '
+        self.assertEqual('1234?678? ILL', Report.get_report_in_line(text))
+
+
+if __name__ == '__main__':
+    unittest.main()
