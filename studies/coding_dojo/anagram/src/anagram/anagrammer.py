554e49dfdd35904bacc5063688a7768900c11cb5
Wesley Ribeiro <wes.rsilva@gmail.com>
2020-05-06 08:44:30 -0300

    Primeiro teste do Kata com TDD

---
+++ studies/coding_dojo/anagram/src/anagram/anagrammer.py
@@ -0,0 +1,8 @@
+class Anagrammer:
+    __slots__ = ['_word']
+
+    def __init__(self, word: str):
+        self._word = word
+
+    def this_is_an_anagram(self, list_of_word: list):
+        return True
---
+++ studies/coding_dojo/anagram/test/test_anagrammer.py
@@ -0,0 +1,18 @@
+import unittest
+
+from studies.coding_dojo.anagram.src.anagram.anagrammer import Anagrammer
+
+
+class AnagrammerTestCase(unittest.TestCase):
+
+    def test_should_indicate_true_if_finding_a_anagram_on_a_list(self):
+        anagrammer = Anagrammer('documenting')
+        self.assertTrue(anagrammer.this_is_an_anagram(['abc', 'ntmeingcudo']))
+
+    def test_should_indicate_false_if_not_finding_a_anagram_on_a_list(self):
+        anagrammer = Anagrammer('documenting')
+        self.assertFalse(anagrammer.this_is_an_anagram(['to', 'do']))
+
+
+if __name__ == '__main__':
+    unittest.main()
