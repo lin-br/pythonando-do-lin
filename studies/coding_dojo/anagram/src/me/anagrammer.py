d12af741d8fe411c685ad55fa876d023fe79b8b6
Wesley Ribeiro <wes.rsilva@gmail.com>
2020-05-13 08:02:14 -0300

    Ajustar nome de método

--- studies/coding_dojo/anagram/src/me/anagrammer.py
+++ studies/coding_dojo/anagram/src/me/anagrammer.py
@@ -5,6 +5,6 @@ class Anagrammer:
         self._word = word
         self._word_sorted = sorted(word)
 
-    def this_is_an_anagram(self, list_of_word: list):
+    def is_has_anagram(self, list_of_word: list):
         list_of_word_sorted = [sorted(word) for word in list_of_word]
         return self._word_sorted in list_of_word_sorted
--- studies/coding_dojo/anagram/tests/test_anagrammer.py
+++ studies/coding_dojo/anagram/tests/test_anagrammer.py
@@ -7,11 +7,11 @@ class TestAnagrammer(unittest.TestCase):
 
     def test_should_indicate_true_if_finding_a_anagram_on_a_list(self):
         anagrammer = Anagrammer('documenting')
-        self.assertTrue(anagrammer.this_is_an_anagram(['abc', 'ntmeingcudo']))
+        self.assertTrue(anagrammer.is_has_anagram(['abc', 'ntmeingcudo']))
 
     def test_should_indicate_false_if_not_finding_a_anagram_on_a_list(self):
         anagrammer = Anagrammer('documenting')
-        self.assertFalse(anagrammer.this_is_an_anagram(['to', 'do']))
+        self.assertFalse(anagrammer.is_has_anagram(['to', 'do']))
 
 
 if __name__ == '__main__':
