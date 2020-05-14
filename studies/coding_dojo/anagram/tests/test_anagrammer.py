9746c99b4f1a9d9af8ff152a6f07743a0caa9a79
Wesley Ribeiro <wes.rsilva@gmail.com>
2020-05-14 09:07:42 -0300

    Adicionar testes do resultado do Anagrammer

--- studies/coding_dojo/anagram/tests/test_anagrammer.py
+++ studies/coding_dojo/anagram/tests/test_anagrammer.py
@@ -13,6 +13,29 @@ class TestAnagrammer(unittest.TestCase):
         anagrammer = Anagrammer('documenting')
         self.assertFalse(anagrammer.is_has_anagram(['to', 'do']))
 
+    def test_should_get_the_result_of_anagrams_with_three_anagrams_given_a_matrix_contains_three_anagrams(self):
+        anagrammer = Anagrammer('maria')
+        matrix = [['ma', 'ria'], ['mm', 'ira'], ['ma', 'ri', 'a'], ['a', 'arim'], ['nothing']]
+        result = anagrammer.get_the_result_of_anagrams(matrix)
+        self.assertEqual(3, result.number_of_anagrams)
+
+    def test_should_get_the_result_of_anagrams_without_anagrams_given_a_matrix_not_contains_anagrams(self):
+        anagrammer = Anagrammer('maria')
+        matrix = [['0', '1'], ['6', '1'], ['9', '34', '8'], ['23', '76'], ['000']]
+        result = anagrammer.get_the_result_of_anagrams(matrix)
+        self.assertEqual(0, result.number_of_anagrams)
+
+    def test_should_the_result_of_anagrams_contains_a_list_with_two_anagrams_equals_topa_and_apot(self):
+        anagrammer = Anagrammer('topa')
+        matrix = [['to', 'pa'], ['tooooop', 'a'], ['t', 'op', 'pa'], ['pot', 'aa'], ['apot']]
+        result = anagrammer.get_the_result_of_anagrams(matrix)
+        self.assertListEqual([['to', 'pa'], ['apot']], result.anagrams)
+
+    def test_should_the_result_of_anagrams_contains_the_word_from_anagram(self):
+        anagrammer = Anagrammer('topa')
+        result = anagrammer.get_the_result_of_anagrams([])
+        self.assertEqual('topa', result.word)
+
 
 if __name__ == '__main__':
     unittest.main()
