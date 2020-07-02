95cdb57303be26eecc521db5cf2945d4e6fc9c49
Wesley Ribeiro <wes.rsilva@gmail.com>
2020-07-02 16:39:28 -0300

    Definir testes

---
+++ studies/idtrust/tests/test_avaliation.py
@@ -0,0 +1,22 @@
+def should_avaliation_not_greater_than_100():
+    pass
+
+
+def should_avaliation_not_less_than_0():
+    pass
+
+
+def should_avaliation_less_than_40_must_fail():
+    pass
+
+
+def should_84_avaliation_have_to_be_85():
+    pass
+
+
+def should_81_avaliation_not_have_to_be_85():
+    pass
+
+
+def should_38_avaliation_not_have_to_be_40_and_must_fail():
+    pass
