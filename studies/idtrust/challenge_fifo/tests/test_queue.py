5a6ae087b39a26719c3970766cb58e7e514326d3
Wesley Ribeiro <wes.rsilva@gmail.com>
2020-07-04 14:02:39 -0300

    Refatorar testes

--- studies/idtrust/challenge_fifo/tests/test_queue.py
+++ studies/idtrust/challenge_fifo/tests/test_queue.py
@@ -1,15 +1,20 @@
+from pytest import fixture
+
 from queue_fifo import Queue
 
 
-def test_should_to_queue_element_x_to_begin_queue():
-    queue = Queue()
+@fixture
+def queue():
+    return Queue()
+
+
+def test_should_to_queue_element_x_to_begin_queue(queue):
     queue.to_queue(10)
     assert queue.length == 1
     assert queue.dequeue() == 10
 
 
-def test_should_dequeue_the_first_element_from_queue():
-    queue = Queue()
+def test_should_dequeue_the_first_element_from_queue(queue):
     queue.to_queue(10)
     queue.to_queue(20)
     queue.to_queue(30)
