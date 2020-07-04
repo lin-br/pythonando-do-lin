95350e95b16a3285badc0f3e8caa74bd50e3d504
Wesley Ribeiro <wes.rsilva@gmail.com>
2020-07-04 14:41:29 -0300

    Resolver teste do tipo de operação "2"

--- studies/idtrust/challenge_fifo/src/operations.py
+++ studies/idtrust/challenge_fifo/src/operations.py
@@ -1,9 +1,14 @@
+from queue_fifo import Queue
+
+
 class Operations:
     __slots__ = ['__queue']
 
-    def __init__(self, queue):
+    def __init__(self, queue: Queue):
         self.__queue = queue
 
     def execute(self, operation: str):
         if operation.startswith('1'):
             self.__queue.enqueue(int(operation.split(' ')[1]))
+        elif operation.startswith('2'):
+            self.__queue.dequeue()
--- studies/idtrust/challenge_fifo/tests/test_operations.py
+++ studies/idtrust/challenge_fifo/tests/test_operations.py
@@ -5,19 +5,26 @@ from queue_fifo import Queue
 
 
 @fixture
-def operations():
-    return Operations(Queue())
+def queue():
+    return Queue()
 
 
-def test_should_operation_1_to_queue_element_x_to_begin_queue():
-    queue = Queue()
-    operations = Operations(queue)
-    operations.execute("1 10")
+@fixture
+def operations(queue):
+    return Operations(queue)
+
+
+def test_should_operation_1_to_queue_element_x_to_begin_queue(operations, queue):
+    operations.execute('1 10')
     assert queue.length == 1
 
 
-def test_should_operation_2_dequeue_the_first_element_from_queue():
-    pass
+def test_should_operation_2_dequeue_the_first_element_from_queue(operations, queue):
+    operations.execute('1 10')
+    operations.execute('1 20')
+    operations.execute('1 30')
+    operations.execute('2')
+    assert queue.length == 2
 
 
 def test_should_operation_3_print_the_first_element_from_queue():
