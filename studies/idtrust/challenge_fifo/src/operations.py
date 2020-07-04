873b5c0dfa93c995d5650d5db8e53caa7df9cd28
Wesley Ribeiro <wes.rsilva@gmail.com>
2020-07-04 15:30:28 -0300

    Refatorar Operations

--- studies/idtrust/challenge_fifo/src/operations.py
+++ studies/idtrust/challenge_fifo/src/operations.py
@@ -9,15 +9,29 @@ class Operations:
 
     def execute(self, operation: str) -> int:
         if operation.startswith('1'):
-            value = int(operation.split(' ')[1])
-            if value < 1 or value > 1000000000:
-                raise ValueError('The input value doesn\'t can less than 1 or greater than 1000000000.')
-            self.__queue.enqueue(int(operation.split(' ')[1]))
+            self.__execute_operation_one(operation)
         elif operation.startswith('2'):
             self.__queue.dequeue()
         elif operation.startswith('3'):
-            if self.__queue.length == 0:
-                return self.__queue.length
-            return self.__queue.print()
+            return self.__execute_operation_three()
         else:
             raise ValueError(f'Not found the operation to [{operation}]')
+
+    def __execute_operation_one(self, operation):
+        value = self.__get_value(operation)
+        if self.__is_invalid_value(value):
+            raise ValueError("The input value doesn't can less than 1 or greater than 1000000000.")
+        self.__queue.enqueue(value)
+
+    def __execute_operation_three(self) -> int:
+        if self.__queue.length == 0:
+            return self.__queue.length
+        return self.__queue.print()
+
+    @staticmethod
+    def __get_value(operation):
+        return int(operation.split(' ')[1])
+
+    @staticmethod
+    def __is_invalid_value(value: int):
+        return value < 1 or value > 1000000000
