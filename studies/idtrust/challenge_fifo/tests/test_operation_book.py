04e383bd0fa18cebc3df7e6ac815d5e051acf02e
Wesley Ribeiro <wes.rsilva@gmail.com>
2020-07-04 14:30:02 -0300

    Separar testes de acordo com as responsabilidades

---
+++ studies/idtrust/challenge_fifo/tests/test_operation_book.py
@@ -0,0 +1,6 @@
+def test_should_the_number_of_operations_greater_than_or_equal_to_1():
+    pass
+
+
+def test_should_the_number_of_operations_less_than_or_equal_to_100000():
+    pass
---
+++ studies/idtrust/challenge_fifo/tests/test_operations.py
@@ -0,0 +1,30 @@
+def test_should_operation_1_to_queue_element_x_to_begin_queue():
+    pass
+
+
+def test_should_operation_2_dequeue_the_first_element_from_queue():
+    pass
+
+
+def test_should_operation_3_print_the_first_element_from_queue():
+    pass
+
+
+def test_should_type_number_greater_than_or_equal_to_1():
+    pass
+
+
+def test_should_type_number_less_than_or_equal_to_3():
+    pass
+
+
+def test_should_the_value_to_queue_greater_than_or_equal_to_1():
+    pass
+
+
+def test_should_the_value_to_queue_less_than_or_equal_to_1000000000():
+    pass
+
+
+def test_should_always_exist_a_valid_value_for_operation_type_number_3():
+    pass
