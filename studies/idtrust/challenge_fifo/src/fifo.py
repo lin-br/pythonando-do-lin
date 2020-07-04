46b287966fd64a297d13e8aed7eb7b8693c53f53
Wesley Ribeiro <wes.rsilva@gmail.com>
2020-07-04 16:45:05 -0300

    Resolver testes iniciais do Book

--- studies/idtrust/challenge_fifo/src/fifo.py
+++ studies/idtrust/challenge_fifo/src/fifo.py
@@ -27,3 +27,30 @@ class Fifo(Queue[int]):
 
     def print(self) -> int:
         return self.__begin.value
+
+
+class FifoString(Queue[str]):
+    __slots__ = ['__begin', '__last']
+
+    def __init__(self):
+        super().__init__()
+        self.__begin = None
+        self.__last = None
+
+    def enqueue(self, text: str):
+        new = Node(text)
+        if self.__begin is None:
+            self.__begin = new
+        elif self.__last is not None:
+            self.__last.after = new
+        self.__last = new
+        super()._increment_size()
+
+    def dequeue(self) -> str:
+        first = self.__begin
+        self.__begin = first.after
+        super()._decrement_size()
+        return first.value
+
+    def print(self) -> str:
+        return self.__begin.value
--- studies/idtrust/challenge_fifo/tests/test_operation_book.py
+++ studies/idtrust/challenge_fifo/tests/test_operation_book.py
@@ -1,6 +1,20 @@
-def test_should_the_number_of_operations_greater_than_or_equal_to_1():
-    pass
+from pytest import fixture, raises
+
+from book import Book
+from fifo import Fifo
+from operations import Operations
 
 
-def test_should_the_number_of_operations_less_than_or_equal_to_100000():
-    pass
+@fixture
+def book():
+    return Book(Operations(Fifo()))
+
+
+def test_should_the_number_of_operations_greater_than_or_equal_to_1(book):
+    with raises(ValueError):
+        book.execute_operations(-1)
+
+
+def test_should_the_number_of_operations_less_than_or_equal_to_100000(book):
+    with raises(ValueError):
+        book.execute_operations(100001)
