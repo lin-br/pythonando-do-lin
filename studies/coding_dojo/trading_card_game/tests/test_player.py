7c5ee46b11160b25c82983df6396bc4098a22966
Wesley Ribeiro <wes.rsilva@gmail.com>
2020-07-07 11:59:23 -0300

    Corrigir teste

--- studies/coding_dojo/trading_card_game/tests/test_player.py
+++ studies/coding_dojo/trading_card_game/tests/test_player.py
@@ -6,14 +6,19 @@ from player import Player
 
 
 @fixture
-def deck():
-    return Deck([
-        Card(0), Card(0), Card(1), Card(1),
-        Card(2), Card(2), Card(2), Card(2),
-        Card(3), Card(3), Card(3), Card(3),
-        Card(4), Card(4), Card(5), Card(5),
-        Card(6), Card(6), Card(7), Card(8)
-    ])
+def list_of_mana():
+    return [
+        0, 0, 1, 1,
+        2, 2, 2, 2,
+        3, 3, 3, 3,
+        4, 4, 5, 5,
+        6, 6, 7, 8
+    ]
+
+
+@fixture
+def deck(list_of_mana):
+    return Deck([Card(number) for number in list_of_mana])
 
 
 def test_should_a_player_begin_with_thirty_health(deck):
@@ -29,27 +34,13 @@ def test_should_a_player_begin_with_a_deck_of_twenty_damage_cards(deck):
     assert len(player.deck) == 20
 
 
-def test_should_a_player_begin_with_a_deck_of_damage_cards_with_following_mana_costs(deck):
+def test_should_a_player_begin_with_a_deck_of_damage_cards_with_following_mana_costs(deck, list_of_mana):
+    list_without_duplicates = list(set(list_of_mana))
     player = Player(deck)
-    for cost, quantity in player.deck.get_number_cards_per_mana():
-        if cost == 0:
-            assert quantity == 2
-        if cost == 1:
-            assert quantity == 2
-        if cost == 2:
-            assert quantity == 3
-        if cost == 3:
-            assert quantity == 4
-        if cost == 4:
-            assert quantity == 2
-        if cost == 5:
-            assert quantity == 2
-        if cost == 6:
-            assert quantity == 2
-        if cost == 7:
-            assert quantity == 1
-        if cost == 8:
-            assert quantity == 1
+    dictionaries = [dictionary for dictionary in player.deck.get_number_cards_per_mana() for number in
+                    list_without_duplicates if dictionary['cost'] == number]
+    for dictionary in dictionaries:
+        assert dictionary['quantity'] == list_of_mana.count(dictionary['cost'])
 
 
 def test_should_a_player_receives_a_three_cards_has_initial_hand(deck):
